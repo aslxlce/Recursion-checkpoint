f27b296ae3e97dfc44687e86f561c7cfc53db322
aslxlce <aslxlce@outlook.com>
2025-01-30 09:44:00 +0100

    ok

--- script.js
+++ script.js
@@ -1,3 +1,34 @@
+//Price switch
+
+function getTicketPriceSwitch(age) {
+    switch (true) {
+        case age <= 12:
+            return 10;
+        case age >= 13 && age <= 17:
+            return 15;
+        default:
+            return 20;
+    }
+}
+
+const userAge = parseInt(prompt("Entrez votre âge :"), 10);
+console.log(`Le prix de votre billet est de ${getTicketPriceSwitch(userAge)} $.`);
+
+//LeapYear
+
+function isLeapYear(year) {
+    if (year % 400 === 0 || (year % 4 === 0 && year % 100 !== 0)) {
+        return true;
+    }
+    return false;
+}
+const year = parseInt(prompt("Entrez une année :"), 10);
+if (isLeapYear(year)) {
+    console.log(`${year} est une année bissextile.`);
+} else {
+    console.log(`${year} n'est pas une année bissextile.`);
+}
+
 // Palindrome recursion
 
 function isPalindrome(word) {
